8e483cfd714f6e974c3161f8bcb22117b82c80f4
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-08 10:39:37 +0800

    Add description field to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208022927) do
+ActiveRecord::Schema.define(version: 20171208023257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171208022927) do
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description", default: "", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
