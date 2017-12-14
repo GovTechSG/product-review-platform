3966d03a4411b1eec00912a2fbeed0d7ad4c2b16
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-14 11:43:27 +0800

    Update service fixture names. Add service review fixtures and tests for service model

--- test/models/service_test.rb
+++ test/models/service_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @service = services(:service_foo)
+  end
+
+  test 'valid service' do
+    assert @service.valid?
+  end
+
+  test 'invalid without name' do
+    @service.name = nil
+    refute @service.valid? 'saved service without a name'
+    assert_not_nil @service.errors[:name]
+  end
+
+  test 'invalid without description' do
+    @service.description = nil
+    refute @service.valid? 'saved service without a description'
+    assert_not_nil @service.errors[:description]
+  end
+
+  test 'has many reviews' do
+    assert_equal 2, @service.reviews.size
+  end
 end
