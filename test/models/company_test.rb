a9c83311bfa1de39d1d722f59918ce1188ba6f51
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-14 11:28:23 +0800

    Add fixtures for companies, products and services. Add model tests for company

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @company = companies(:one)
+  end
+
+  test 'valid company' do
+    assert @company.valid?
+  end
+
+  test 'invalid without name' do
+    @company.name = nil
+    refute @company.valid? 'saved company without a name'
+    assert_not_nil @company.errors[:name]
+  end
+
+  test 'invalid without UEN' do
+    @company.UEN = nil
+    refute @company.valid? 'saved company without a UEN'
+    assert_not_nil @company.errors[:UEN]
+  end
+
+  test 'invalid without aggregate_score' do
+    @company.aggregate_score = nil
+    refute @company.valid? 'saved company without an aggregate_score'
+    assert_not_nil @company.errors[:aggregate_score]
+  end
+
+  test 'has many products' do
+    assert_equal 2, @company.products.size
+  end
+
+  test 'has many services' do
+    assert_equal 2, @company.services.size
+  end
 end
