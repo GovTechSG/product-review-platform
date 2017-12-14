3d8b30a03bb04a1a9a3c3f340ad0572871fb6edd
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-14 13:47:09 +0800

    Add dependent: :destroy for models with has_many relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :company
-  has_many :reviews, as: :reviewable
+  has_many :reviews, as: :reviewable, dependent: :destroy
 
   validates_presence_of :name, :description, :company
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ApplicationRecord
   belongs_to :agency
   belongs_to :reviewable, polymorphic: true
 
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates_presence_of :score, :content, :agency, :reviewable
 end
