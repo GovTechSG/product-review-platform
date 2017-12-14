e607f3e912c3883e5e3eb3fbeacefb89f0a4d6bb
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-14 11:40:07 +0800

    Add product review fixtures. Add tests for product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @product = products(:pivotal_tracker)
+  end
+
+  test 'valid product' do
+    assert @product.valid?
+  end
+
+  test 'invalid without name' do
+    @product.name = nil
+    refute @product.valid? 'saved product without a name'
+    assert_not_nil @product.errors[:name]
+  end
+
+  test 'invalid without description' do
+    @product.description = nil
+    refute @product.valid? 'saved product without a description'
+    assert_not_nil @product.errors[:description]
+  end
+
+  test 'has many reviews' do
+    assert_equal 2, @product.reviews.size
+  end
 end
