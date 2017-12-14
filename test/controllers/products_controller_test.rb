8d0e5edec2ed7238b453c48abf6b65fea7ccc473
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-14 13:57:01 +0800

    Update tests for ProductController

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,17 +2,25 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @product = products(:one)
+    @product = products(:pivotal_tracker)
+    @company = @product.company
   end
 
   test "should get index" do
-    get products_url, as: :json
+    get company_products_url(@company.id), as: :json
+
     assert_response :success
+    assert_equal @company.products.to_json, response.body
   end
 
   test "should create product" do
+    product = {
+      name: "Product Foo",
+      description: "Lorem ipsum",
+      company_id: @company.id
+    }
     assert_difference('Product.count') do
-      post products_url, params: { product: {  } }, as: :json
+      post company_products_url(@company.id), params: { product: product }, as: :json
     end
 
     assert_response 201
@@ -21,10 +29,16 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should show product" do
     get product_url(@product), as: :json
     assert_response :success
+    assert_equal @product.to_json, response.body
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: {  } }, as: :json
+    updated = {
+      name: "New Product",
+      description: "New description",
+      company_id: @company.id
+    }
+    patch product_url(@product), params: { product: updated }, as: :json
     assert_response 200
   end
 
