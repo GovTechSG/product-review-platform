60bc825819e2e5f1941a9f5d30b94ea39107362a
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-14 13:26:06 +0800

    Add validation for uniqueness of agency_id and review_id. Add tests for Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,5 @@ class Like < ApplicationRecord
   belongs_to :review
 
   validates_presence_of :agency, :review
+  validates_uniqueness_of :agency_id, uniqueness: { scope: :review_id }
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @like = likes(:one)
+    @like_two = likes(:two)
+  end
+
+  test 'invalid if existing like has the same review and agency' do
+    @like.review_id = @like_two.review.id
+    refute @like.valid? "saved like with existing like's review and agency"
+    assert_not_nil @like.errors[:agency_id]
+  end
 end
