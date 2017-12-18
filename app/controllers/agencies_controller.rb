6a79fdaacb8d5cc7271f3501be873e09ce690984
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-18 10:50:07 +0800

    Add swagger for Agency

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -1,6 +1,107 @@
 class AgenciesController < ApplicationController
   before_action :set_agency, only: [:show, :update, :destroy]
 
+  include Swagger::Blocks
+
+  swagger_path '/agencies' do
+    operation :get do
+      key :description, 'Returns list of all agencies'
+      key :operationId, 'findAgencies'
+      key :produces, [
+        'application/json'
+      ]
+      response 200 do
+        schema type: :array do
+          items do
+            key :'$ref', :Agency
+          end
+        end
+      end
+    end
+    operation :post do
+      key :description, 'Creates a new agency'
+      key :operationId, 'addAgency'
+      key :produces, [
+        'application/json'
+      ]
+      parameter do
+        key :name, :agency
+        key :in, :body
+        key :description, 'Agency to create'
+        key :required, true
+        schema do
+          key :'$ref', :AgencyInput
+        end
+      end
+      response 200 do
+        schema do
+          key :'$ref', :Agency
+        end
+      end
+      response 422 do
+        key :description, 'Unprocessable Entity'
+        schema do
+          key :'$ref', :SwaggerErrorModel
+        end
+      end
+    end
+  end
+
+  swagger_path '/agencies/{id}' do
+    parameter do
+      key :name, :id
+      key :in, :path
+      key :description, 'ID of agency'
+      key :required, true
+      key :type, :integer
+      key :format, :int64
+    end
+    operation :put do
+      key :description, 'Update an agency'
+      key :operationId, 'updateAgency'
+      key :produces, [
+        'application/json'
+      ]
+      parameter do
+        key :name, :agency
+        key :in, :body
+        key :description, 'Agency to update'
+        key :required, true
+        schema do
+          key :'$ref', :AgencyInput
+        end
+      end
+      response 200 do
+        schema do
+          key :'$ref', :Agency
+        end
+      end
+      response 422 do
+        key :description, 'Unprocessable Entity'
+        schema do
+          key :'$ref', :SwaggerErrorModel
+        end
+      end
+    end
+    operation :get do
+      key :description, 'Returns an agency'
+      key :operationId, 'findAgencyById'
+      key :produces, [
+        'application/json'
+      ]
+      response 200 do
+        schema do
+          key :'$ref', :Agency
+        end
+      end
+    end
+    operation :delete do
+      key :description, 'Deletes a single agency'
+      key :operationId, 'deleteAgency'
+      response 204
+    end
+  end
+
   # GET /agencies
   def index
     @agencies = Agency.all
--- app/models/agency.rb
+++ app/models/agency.rb
@@ -4,4 +4,39 @@ class Agency < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates_presence_of :name, :email, :number
+
+  include Swagger::Blocks
+
+  swagger_schema :Agency do
+    key :type, :object
+    key :required, [:id, :name, :email, :number]
+
+    property :id do
+      key :type, :integer
+      key :format, :int64
+    end
+
+    property :name do
+      key :type, :string
+    end
+
+    property :email do
+      key :type, :string
+    end
+
+    property :number do
+      key :type, :string
+    end
+  end
+
+  swagger_schema :AgencyInput do
+    allOf do
+      schema do
+        property :agency do
+          key :type, :object
+          key :'$ref', :Agency
+        end
+      end
+    end
+  end
 end
