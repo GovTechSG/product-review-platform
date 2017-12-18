b3ae2bf9dbff5e6197b2ba727f7259ac0cc760d0
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-18 15:40:14 +0800

    Refactor swagger docs out of Product resources

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,53 +1,8 @@
 class Product < ApplicationRecord
+  include SwaggerDocs::Product
+
   belongs_to :company
   has_many :reviews, as: :reviewable, dependent: :destroy
 
   validates_presence_of :name, :description, :company
-
-  include Swagger::Blocks
-
-  swagger_schema :Product do
-    key :type, :object
-    key :required, [:id, :name, :description, :company_id]
-
-    property :id do
-      key :type, :integer
-      key :format, :int64
-    end
-
-    property :name do
-      key :type, :string
-    end
-
-    property :description do
-      key :type, :string
-    end
-
-    property :company_id do
-      key :type, :integer
-      key :format, :int64
-    end
-  end
-
-  swagger_schema :ProductInput do
-    allOf do
-      schema do
-        property :product do
-          key :type, :object
-          property :name do
-            key :type, :string
-          end
-
-          property :description do
-            key :type, :string
-          end
-
-          property :company_id do
-            key :type, :integer
-            key :format, :int64
-          end
-        end
-      end
-    end
-  end
 end
