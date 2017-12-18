6c2769a176aab4a365603b7f788a9c76ab4d9297
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-18 15:35:31 +0800

    Refactor swagger docs out of Agency resources

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,52 +1,9 @@
 class Agency < ApplicationRecord
+  include SwaggerDocs::Agency
+
   has_many :reviews, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   validates_presence_of :name, :email, :number
-
-  include Swagger::Blocks
-
-  swagger_schema :Agency do
-    key :type, :object
-    key :required, [:id, :name, :email, :number]
-
-    property :id do
-      key :type, :integer
-      key :format, :int64
-    end
-
-    property :name do
-      key :type, :string
-    end
-
-    property :email do
-      key :type, :string
-    end
-
-    property :number do
-      key :type, :string
-    end
-  end
-
-  swagger_schema :AgencyInput do
-    allOf do
-      schema do
-        property :agency do
-          key :type, :object
-          property :name do
-            key :type, :string
-          end
-
-          property :email do
-            key :type, :string
-          end
-
-          property :number do
-            key :type, :string
-          end
-        end
-      end
-    end
-  end
 end
