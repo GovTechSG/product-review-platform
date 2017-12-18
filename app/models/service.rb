e6e3b887ecf8b3f5c780272ffe3cdffb547535bb
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-18 13:32:07 +0800

    Add swagger for service

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,51 @@ class Service < ApplicationRecord
   has_many :reviews, as: :reviewable, dependent: :destroy
 
   validates_presence_of :name, :description, :company
+
+  include Swagger::Blocks
+
+  swagger_schema :Service do
+    key :type, :object
+    key :required, [:id, :name, :description, :company_id]
+
+    property :id do
+      key :type, :integer
+      key :format, :int64
+    end
+
+    property :name do
+      key :type, :string
+    end
+
+    property :description do
+      key :type, :string
+    end
+
+    property :company_id do
+      key :type, :integer
+      key :format, :int64
+    end
+  end
+
+  swagger_schema :ServiceInput do
+    allOf do
+      schema do
+        property :service do
+          key :type, :object
+          property :name do
+            key :type, :string
+          end
+
+          property :description do
+            key :type, :string
+          end
+
+          property :company_id do
+            key :type, :integer
+            key :format, :int64
+          end
+        end
+      end
+    end
+  end
 end
