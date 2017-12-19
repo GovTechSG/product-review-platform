3a0ea4471c779aefb24a1aa300bded30cd2b5af3
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-19 17:31:40 +0800

    Add user authentication to services controller. Update tests for services controller

--- test/controllers/services_controller_test.rb
+++ test/controllers/services_controller_test.rb
@@ -4,13 +4,35 @@ class ServicesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @service = services(:service_foo)
     @company = @service.company
+    @auth_headers = users(:one).create_new_auth_token
+  end
+
+  test "should not get index if not signed in" do
+    get company_services_url(@company.id), as: :json
+
+    assert_response 401
+    assert_not_signed_in_error response.body
   end
 
   test "should get index" do
-    get company_services_url(@company.id), as: :json
+    get company_services_url(@company.id), headers: @auth_headers, as: :json
 
     assert_response :success
     assert_equal @company.services.to_json, response.body
+  end
+
+  test "should not create service if not signed in" do
+    service = {
+      name: "New Service",
+      description: "New description",
+      company_id: @company.id
+    }
+    assert_no_difference('Service.count') do
+      post company_services_url(@company.id), params: { service: service }, as: :json
+    end
+
+    assert_response 401
+    assert_not_signed_in_error response.body
   end
 
   test "should create service" do
@@ -20,17 +42,35 @@ class ServicesControllerTest < ActionDispatch::IntegrationTest
       company_id: @company.id
     }
     assert_difference('Service.count') do
-      post company_services_url(@company.id), params: { service: service }, as: :json
+      post company_services_url(@company.id), params: { service: service }, headers: @auth_headers, as: :json
     end
 
     assert_response 201
   end
 
-  test "should show service" do
+  test "should not show service if not signed in" do
     get service_url(@service), as: :json
+
+    assert_response 401
+    assert_not_signed_in_error response.body
+  end
+
+  test "should show service" do
+    get service_url(@service), headers: @auth_headers, as: :json
 
     assert_response :success
     assert_equal @service.to_json, response.body
+  end
+
+  test "should not update service if not signed in" do
+    updated = {
+      name: "New Service",
+      description: "New description"
+    }
+    patch service_url(@service), params: { service: updated }, as: :json
+
+    assert_response 401
+    assert_not_signed_in_error response.body
   end
 
   test "should update service" do
@@ -38,13 +78,22 @@ class ServicesControllerTest < ActionDispatch::IntegrationTest
       name: "New Service",
       description: "New description"
     }
-    patch service_url(@service), params: { service: updated }, as: :json
+    patch service_url(@service), params: { service: updated }, headers: @auth_headers, as: :json
     assert_response 200
+  end
+
+  test "should not destroy service if not signed in" do
+    assert_no_difference('Service.count') do
+      delete service_url(@service), as: :json
+    end
+
+    assert_response 401
+    assert_not_signed_in_error response.body
   end
 
   test "should destroy service" do
     assert_difference('Service.count', -1) do
-      delete service_url(@service), as: :json
+      delete service_url(@service), headers: @auth_headers, as: :json
     end
 
     assert_response 204
