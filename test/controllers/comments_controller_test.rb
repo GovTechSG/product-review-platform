5066a27ecfdb0c3df04e53cc4f79d852a052874f
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-21 11:04:07 +0800

    Update comments controller test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -10,7 +10,10 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
     get review_comments_url(@review.id), as: :json
 
     assert_response :success
-    assert_equal @review.comments.to_json, response.body
+    # Call :agency method for each comment
+    expected = @review.comments.as_json
+    expected.each { |comment| comment["agency"] = @comment.agency }
+    assert_equal expected.to_json, response.body
   end
 
   test "should create comment" do
