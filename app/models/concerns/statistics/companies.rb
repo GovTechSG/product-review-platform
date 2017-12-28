94b694a3299f312c70a8caa73e5c92d7decd6b50
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-28 15:23:57 +0800

    Simplify reviews_count method for companies concern

--- app/models/concerns/statistics/companies.rb
+++ app/models/concerns/statistics/companies.rb
@@ -3,15 +3,7 @@ module Statistics::Companies
 
   included do
     def reviews_count
-      num_product_reviews = 0
-      self.products.each do |pdt|
-        num_product_reviews += pdt.reviews.count
-      end
-      num_service_reviews = 0
-      self.services.each do |svc|
-        num_service_reviews += svc.reviews.count
-      end
-      num_product_reviews + num_service_reviews
+      self.products.reduce(0) { |accum, p| accum + p.reviews.count } + self.services.reduce(0) { |accum, s| accum + s.reviews.count }
     end
   end
 end
