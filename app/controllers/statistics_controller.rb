22d67dc3cbb5f637d5a8691263acf4b0d628ee67
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2017-12-28 10:05:22 +0800

    Add authentication for statistics controller. Update statistics controller test

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,4 +1,5 @@
 class StatisticsController < ApplicationController
+  before_action :authenticate_user!
   def index
     render :json => {
       reviews: Review.count,
--- test/controllers/statistics_controller_test.rb
+++ test/controllers/statistics_controller_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class StatisticsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  setup do
+    @auth_headers = users(:one).create_new_auth_token
+  end
+
+  test "should not get index if not signed in" do
     get statistics_url, as: :json
+
+    assert_response 401
+    assert_not_signed_in_error response.body
+  end
+
+  test "should get index" do
+    get statistics_url, headers: @auth_headers, as: :json
 
     assert_response :success
     body = JSON.parse(response.body)
