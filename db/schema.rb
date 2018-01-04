f73a1b48c85d87383e2aba624d81b5fc6137f888
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2018-01-04 10:48:38 +0800

    Add description field for companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103030346) do
+ActiveRecord::Schema.define(version: 20180103082733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20180103030346) do
     t.decimal "aggregate_score", default: "0.0", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description", default: "", null: false
     t.index ["UEN"], name: "index_companies_on_UEN", unique: true
   end
 
