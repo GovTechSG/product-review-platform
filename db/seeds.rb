2402bab3c1660df6dfd8472e4738f99fae233e40
Shaun Thium Wai Hoh <shaun.thium@gmail.com>
2018-01-08 10:35:27 +0800

    Add seeds for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,107 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# For demo purposes
+User.create!(email: "user1@foo.com", password: "password1", password_confirmation: "password1", confirm_success_url: "localhost:3000")
+User.first.confirm
+company_names = [
+  "Pivotal Software",
+  "Microsoft",
+  "Oracle",
+  "SAP",
+  "salesforce.com",
+  "ThoughtWorks",
+  "ADP",
+  "Zendesk",
+  "Hubspot",
+  "RingCentral"
+]
+UENs = [
+  "984208875",
+  "984208874",
+  "984208873",
+  "984208872",
+  "584208374",
+  "384208875",
+  "284208874",
+  "484208873",
+  "844208872",
+  "574208374",
+]
+
+lorem_ipsum = "Vestibulum nec turpis vestibulum, feugiat mi at, egestas ex. Proin non enim mollis lacus pulvinar laoreet et quis augue. Nam nec magna at leo ultrices auctor. Ut interdum a neque eget malesuada. Phasellus vel velit pulvinar tellus cursus pharetra vehicula in lectus. Nulla viverra erat sed viverra viverra. Aliquam at condimentum nisl, eget ornare turpis. Nulla sollicitudin efficitur tortor at imperdiet."
+10.times do |i|
+  Company.create!(name: company_names[i], UEN: UENs[i], aggregate_score: 4.2, description: lorem_ipsum)
+end
+# c_1.products.create!(name: "Step2 Children's Chair", description: "A great chair for your child!")
+# c_1.products.create!(name: "Rachio Router", description: "The best router for your money")
+# c_1.products.create!(name: "ChengXinTu Hiking Backpack", description: "Get ready to summit Mount Everest.")
+# c_1.services.create!(name: "Pivotal Car Mechanics", description: "Lowest prices all around")
+# c_1.services.create!(name: "Greenplum Hand Washers", description: "Cleanest hands")
+# c_1.services.create!(name: "Carpenters R'Us", description: "Got a fixer-upper? Look no further!")
+
+Agency.create!(name: "MTI", email: "agency_1@foo.com", number: "51512234")
+Agency.create!(name: "IES", email: "agency_2@foo.com", number: "51512236")
+Agency.create!(name: "SPRING Singapore", email: "agency_3@foo.com", number: "71512234")
+Agency.create!(name: "EDB", email: "agency_4@foo.com", number: "51512235")
+Agency.create!(name: "IMDA", email: "agency_5@foo.com", number: "51512237")
+
+(1..10).each do |i|
+  c = Company.find(i)
+  c.products.create!(name: "PivotalTracker", description: lorem_ipsum)
+  c.products.create!(name: "Cloud Foundry", description: lorem_ipsum)
+  c.products.create!(name: "Greenplum", description: lorem_ipsum)
+  c.services.create!(name: "Spring Framework", description: lorem_ipsum)
+  c.services.create!(name: "Agile Development", description: lorem_ipsum)
+end
+
+SMEs = [
+  "Grab",
+  "Carousell",
+  "Ninja Van",
+  "Redmart",
+  "ShopBack",
+  "Shopee",
+  "SEA"
+]
+
+SME_UENs = [
+  "332244643",
+  "332244633",
+  "332254643",
+  "632244643",
+  "442244643",
+  "447244643",
+  "447212643"
+]
+
+7.times do |i|
+  Company.create!(name: SMEs[i], UEN: SME_UENs[i], aggregate_score: 4.2, description: lorem_ipsum)
+end
+
+Product.all.each do |p|
+  p.reviews.create!(score: 4, content: lorem_ipsum, company_id: 11, strengths: ["Empathetic", "Quality Tools & Materials", "Skilful & Knowledgeable"])
+  p.reviews.create!(score: 1, content: lorem_ipsum, company_id: 12, strengths: ["Explanation of Project and Resources", "Skilful & Knowledgeable"])
+  p.reviews.create!(score: 3, content: lorem_ipsum, company_id: 13, strengths: ["Reliable", "Explanation of Project and Resources", "Responsive to Messages"])
+  p.reviews.create!(score: 3, content: lorem_ipsum, company_id: 14, strengths: ["Empathetic", "Skilful & Knowledgeable", "Responsive to Messages"])
+  p.reviews.create!(score: 4, content: lorem_ipsum, company_id: 15, strengths: ["Reliable", "Responsive to Messages"])
+  p.reviews.create!(score: 5, content: lorem_ipsum, company_id: 16, strengths: ["Explanation of Project and Resources", "Skilful & Knowledgeable"])
+end
+
+Service.all.each do |s|
+  s.reviews.create!(score: 4, content: lorem_ipsum, company_id: 11, strengths: ["Reliable", "Responsive to Messages"])
+  s.reviews.create!(score: 2, content: lorem_ipsum, company_id: 14, strengths: ["Empathetic", "Skilful & Knowledgeable", "Responsive to Messages"])
+  s.reviews.create!(score: 5, content: lorem_ipsum, company_id: 15, strengths: ["Empathetic", "Quality Tools & Materials", "Skilful & Knowledgeable"])
+end
+
+Review.all.each_with_index do |r, i|
+  r.comments.create!(content: lorem_ipsum, agency_id: 1)
+  r.comments.create!(content: lorem_ipsum, agency_id: 2)
+  r.comments.create!(content: lorem_ipsum, agency_id: 3)
+  r.comments.create!(content: lorem_ipsum, agency_id: 2)
+  r.comments.create!(content: lorem_ipsum, agency_id: 4)
+  r.comments.create!(content: lorem_ipsum, agency_id: 5)
+
+  r.likes.create!(agency_id: 1)
+  r.likes.create!(agency_id: 2)
+  r.likes.create!(agency_id: 3)
+  r.likes.create!(agency_id: 4)
+  r.likes.create!(agency_id: 5)
+end
