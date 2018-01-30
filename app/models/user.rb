9e0fe8330b2fa199214f8755205a8f7b8169d42e
gohkokboon <guowen4567@gmail.com>
2018-01-30 16:45:44 +0800

    removed confirmable from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   # :omniauthable, :rememberable
   devise :database_authenticatable, :registerable,
-          :recoverable, :trackable, :validatable,
-          :confirmable
+          :recoverable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
