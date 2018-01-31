5e8657c53661b58f67ed18efe1cba70fbb636f4b
Wynn Chen <wynn987@gmail.com>
2018-01-31 16:00:22 +0800

    added comments for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
-# app/controllers/api_controller.rb
+# API routes go here
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include RenderErrors
 end
 
+# Front-end routes go here
 class BaseController < ActionController::Base
 end
