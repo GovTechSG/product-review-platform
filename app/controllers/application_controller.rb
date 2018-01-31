29252dca84d57833df21d1691b120030fd5534eb
Wynn Chen <wynn987@gmail.com>
2018-01-31 15:59:26 +0800

    seperate front facing contraoller with backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
+# app/controllers/api_controller.rb
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include RenderErrors
+end
+
+class BaseController < ActionController::Base
 end
