0daf8ecc9ad03d4ce00bd8d512e246a0b4829613
gohkokboon <guowen4567@gmail.com>
2018-02-01 15:31:03 +0800

    Created and seeded multi-tenant db for playground

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # Front facing routes inherit this
 class ApplicationController < ActionController::Base
+  before_action :check_host
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include RenderErrors
+
+  def check_host
+    puts "Where is it from: " , request.headers['location']
+    if request.headers['location'] == "playground"
+      Apartment::Tenant.switch!("playground")
+    else
+      Apartment::Tenant.switch!
+    end
+  end
 end
 
