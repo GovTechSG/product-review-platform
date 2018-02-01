98a262058a69ffc4cfd8505a1dc9b9396c76ca37
gohkokboon <guowen4567@gmail.com>
2018-02-01 15:31:03 +0800

    Created and seeded multi-tenant db for playground

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,16 @@
 class ApplicationController < ActionController::Base
   before_action :check_host
 
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  include RenderErrors
-
   PLAY_ENV = '"playground"'
 
   def check_host
     puts "Where is it from: " , request.headers['location']
-    if request.headers['location'].eql? PLAY_ENV
-      Apartment::Tenant.switch!("playground")
-      puts Apartment::Tenant.current
-    else
-      Apartment::Tenant.switch!
-      puts Apartment::Tenant.current
+      if request.headers['location'].eql? PLAY_ENV
+        Apartment::Tenant.switch!("playground")
+        puts Apartment::Tenant.current
+      else
+        Apartment::Tenant.switch!
+        puts Apartment::Tenant.current
     end
   end
 end
