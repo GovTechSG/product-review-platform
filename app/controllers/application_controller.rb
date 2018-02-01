2f235e3c4758b68c799b0120bfb337c215a349c0
gohkokboon <guowen4567@gmail.com>
2018-02-01 15:49:23 +0800

    Added condition to check env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,16 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   include RenderErrors
 
+  PLAY_ENV = '"playground"'
+
   def check_host
     puts "Where is it from: " , request.headers['location']
-    if request.headers['location'] == "playground"
+    if request.headers['location'].eql? PLAY_ENV
       Apartment::Tenant.switch!("playground")
+      puts Apartment::Tenant.current
     else
       Apartment::Tenant.switch!
+      puts Apartment::Tenant.current
     end
   end
 end
