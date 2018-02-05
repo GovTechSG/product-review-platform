16ab292ad93d13931aa17d4197be27e8468b66c4
gohkokboon <guowen4567@gmail.com>
2018-02-05 11:39:59 +0800

    Added (www) schema for minitest to resolve build failed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
   PLAY_ENV = '"playground"'
 
   def check_host
-    puts "Where is it from: " , request.headers['location']
-      if request.headers['location'].eql? PLAY_ENV
-        Apartment::Tenant.switch!("playground")
-        puts Apartment::Tenant.current
-      else
-        Apartment::Tenant.switch!
-        puts Apartment::Tenant.current
+
+    if request.headers['location'].eql? PLAY_ENV
+      Apartment::Tenant.switch!("playground")
+      puts Apartment::Tenant.current
+    else
+      Apartment::Tenant.switch!
+      puts Apartment::Tenant.current
     end
   end
 end
