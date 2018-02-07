64ae191204a8e6f2c98e7181b75be997a2767bfc
Wynn Chen <wynn987@gmail.com>
2018-02-07 16:14:34 +0800

    fix rubocop offense and add test for app model

--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe App, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "authenticate with valid params" do
+    it "returns app" do
+      @app = create(:app)
+      @response = @app.class.authenticate(@app.name, @app.password)
+
+      expect(@response).to match(@app)
+    end
+  end
+
+  describe "authenticate with invalid params" do
+    it "returns app" do
+      @app = build(:app)
+      @response = @app.class.authenticate(@app.name, @app.password)
+
+      expect(@response).to match(nil)
+    end
+  end
 end
