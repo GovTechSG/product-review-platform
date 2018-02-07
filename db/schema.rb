44ee206e73caf2419f80f93be1354f6aa4be96d1
sturdek <kwancheng.lai@gmail.com>
2018-02-07 15:02:39 +0800

    undo schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -148,34 +148,6 @@ ActiveRecord::Schema.define(version: 20180202030134) do
     t.index ["company_id"], name: "index_services_on_company_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "name"
-    t.string "nickname"
-    t.string "image"
-    t.string "email"
-    t.json "tokens"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
-   end
-
   add_foreign_key "comments", "agencies"
   add_foreign_key "comments", "reviews"
   add_foreign_key "likes", "agencies"
