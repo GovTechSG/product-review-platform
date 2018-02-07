a78b8624e8795071cc57f2d55d6e98ff9b7d7f1b
Wynn Chen <wynn987@gmail.com>
2018-02-07 16:14:34 +0800

    fix rubocop offense and add test for app model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,7 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
          authentication_keys: [:email]
 end
