48e15102b6bfdca6bd162d730f2afcd6d6cb3f7d
Wynn Chen <wynn987@gmail.com>
2018-02-07 15:37:34 +0800

    added code for sign up

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module ProductReviewPlatform
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.app_generators.scaffold_controller = :scaffold_controller
 
     # added because api has a frontend
 
