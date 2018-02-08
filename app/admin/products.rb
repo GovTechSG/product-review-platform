8a94257ffe31b655d99dba7aa2a24bc076a0edb0
Wynn Chen <wynn987@gmail.com>
2018-02-08 10:47:06 +0800

    added views for activeadmin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,15 +1,14 @@
 ActiveAdmin.register Product do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :name, :description, :company_id
 
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :company_id,
+              as: :select,
+              collection: Company.all.map { |company| [company.name, company.id] }
+    end
+    f.actions
+  end
 end
