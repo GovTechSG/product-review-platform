d2eadcd1df43a77dca9d741bc5159d60cd780417
Wynn Chen <wynn987@gmail.com>
2018-02-08 10:47:06 +0800

    added views for activeadmin

--- app/admin/agencies.rb
+++ app/admin/agencies.rb
@@ -1,15 +1,12 @@
 ActiveAdmin.register Agency do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :name, :email, :number
 
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :number
+    end
+    f.actions
+  end
 end
