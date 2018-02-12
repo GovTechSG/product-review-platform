8f9bb231b36e6dca4c010638ec6c389ff58877cd
Wynn Chen <wynn987@gmail.com>
2018-02-12 14:59:04 +0800

    fixed missing field and partial edit form

--- app/admin/apps.rb
+++ app/admin/apps.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register App do
-  permit_params :name, :password, :password_confirmation
+  permit_params :name, :password, :password_confirmation, :description
 
   index do
     selectable_column
     id_column
     column :name
+    column :description
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -12,6 +13,7 @@ ActiveAdmin.register App do
   end
 
   filter :name
+  filter :description
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
@@ -19,9 +21,24 @@ ActiveAdmin.register App do
   form do |f|
     f.inputs do
       f.input :name
+      f.input :description
       f.input :password
       f.input :password_confirmation
     end
     f.actions
+  end
+
+  controller do
+    def update
+      if params[:app][:password].blank?
+        params[:app].delete("password")
+        params[:app].delete("password_confirmation")
+      end
+      if params[:app][:email].blank?
+        params[:app].delete("email")
+        params[:app].delete("email_confirmation")
+      end
+      super
+    end
   end
 end
