f2477ef20d903b07024d8d788cbaf28ce95c736e
Kavan Koh <kavanohk@gmail.com>
2018-02-13 18:28:46 +0800

    fix lint warnings

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 require 'support/api_login_helper'
 
 RSpec.describe ProductsController, type: :controller do
-
-  let(:valid_attributes) {
+  let(:valid_attributes) do
     build(:product).attributes
-  }
+  end
 
-  let(:invalid_attributes) {
+  let(:invalid_attributes) do
     build(:product, name: nil).attributes
-  }
+  end
 
   let(:valid_session) {}
 
@@ -32,7 +31,7 @@ RSpec.describe ProductsController, type: :controller do
     describe "GET #show" do
       it "returns a success response", authorized: true do
         product = Product.create! valid_attributes
-        get :show, params: {id: product.to_param}
+        get :show, params: { id: product.to_param }
         expect(response).to be_success
       end
     end
@@ -42,15 +41,15 @@ RSpec.describe ProductsController, type: :controller do
         it "creates a new Product", authorized: true do
           company = create(:company)
 
-          expect {
-            post :create, params: { product: valid_attributes,company_id: company.id }
-          }.to change(Product, :count).by(1)
+          expect do
+            post :create, params: { product: valid_attributes, company_id: company.id }
+          end.to change(Product, :count).by(1)
         end
 
         it "renders a JSON response with the new product", authorized: true do
           company = create(:company)
 
-          post :create, params: { product: valid_attributes,company_id: company.id }
+          post :create, params: { product: valid_attributes, company_id: company.id }
           expect(response).to have_http_status(:created)
           expect(response.content_type).to eq('application/json')
           expect(response.location).to eq(product_url(Product.last))
@@ -70,14 +69,14 @@ RSpec.describe ProductsController, type: :controller do
 
     describe "PUT #update" do
       context "with valid params" do
-        let(:new_attributes) {
+        let(:new_attributes) do
           attributes_for(:product)
-        }
+        end
 
         it "updates the requested product", authorized: true do
           product = Product.create! valid_attributes
 
-          put :update, params: {id: product.to_param, product: new_attributes}, session: valid_session
+          put :update, params: { id: product.to_param, product: new_attributes }, session: valid_session
           product.reload
           expect(product.name).to eq(new_attributes[:name])
           expect(product.description).to eq(new_attributes[:description])
@@ -86,7 +85,7 @@ RSpec.describe ProductsController, type: :controller do
         it "renders a JSON response with the product", authorized: true do
           product = Product.create! valid_attributes
 
-          put :update, params: {id: product.to_param, product: valid_attributes}, session: valid_session
+          put :update, params: { id: product.to_param, product: valid_attributes }, session: valid_session
           expect(response).to have_http_status(:ok)
           expect(response.content_type).to eq('application/json')
         end
@@ -96,7 +95,7 @@ RSpec.describe ProductsController, type: :controller do
         it "renders a JSON response with errors for the product", authorized: true do
           product = Product.create! valid_attributes
 
-          put :update, params: {id: product.to_param, product: invalid_attributes}, session: valid_session
+          put :update, params: { id: product.to_param, product: invalid_attributes }, session: valid_session
           expect(response).to have_http_status(:unprocessable_entity)
           expect(response.content_type).to eq('application/json')
         end
@@ -106,9 +105,9 @@ RSpec.describe ProductsController, type: :controller do
     describe "DELETE #destroy" do
       it "destroys the requested product", authorized: true do
         product = Product.create! valid_attributes
-        expect {
-          delete :destroy, params: {id: product.to_param}, session: valid_session
-        }.to change(Product, :count).by(-1)
+        expect do
+          delete :destroy, params: { id: product.to_param }, session: valid_session
+        end.to change(Product, :count).by(-1)
       end
     end
   end
@@ -126,7 +125,7 @@ RSpec.describe ProductsController, type: :controller do
     describe "GET #show" do
       it "returns an unauthorized response", authorized: false do
         product = Product.create! valid_attributes
-        get :show, params: {id: product.to_param}
+        get :show, params: { id: product.to_param }
 
         expect_unauthorized
       end
@@ -136,29 +135,29 @@ RSpec.describe ProductsController, type: :controller do
       it "does not create a new Product", authorized: false do
         company = create(:company)
 
-        expect {
-          post :create, params: { product: valid_attributes,company_id: company.id }
-        }.to change(Product, :count).by(0)
+        expect do
+          post :create, params: { product: valid_attributes, company_id: company.id }
+        end.to change(Product, :count).by(0)
       end
 
       it "returns an unauthorized response", authorized: false do
         company = create(:company)
 
-        post :create, params: { product: valid_attributes,company_id: company.id }
+        post :create, params: { product: valid_attributes, company_id: company.id }
         expect_unauthorized
       end
     end
 
     describe "PUT #update" do
-      let(:new_attributes) {
+      let(:new_attributes) do
         attributes_for(:product)
-      }
+      end
 
       it "does not update the requested product", authorized: false do
         product = Product.create! valid_attributes
         current_attributes = product.attributes
 
-        put :update, params: {id: product.to_param, product: new_attributes}, session: valid_session
+        put :update, params: { id: product.to_param, product: new_attributes }, session: valid_session
         product.reload
         expect(product.name).to eq(current_attributes["name"])
         expect(product.description).to eq(current_attributes["description"])
@@ -167,7 +166,7 @@ RSpec.describe ProductsController, type: :controller do
       it "returns an unauthorized response", authorized: false do
         product = Product.create! valid_attributes
 
-        put :update, params: {id: product.to_param, product: valid_attributes}, session: valid_session
+        put :update, params: { id: product.to_param, product: valid_attributes }, session: valid_session
         expect_unauthorized
       end
     end
@@ -175,15 +174,15 @@ RSpec.describe ProductsController, type: :controller do
     describe "DELETE #destroy" do
       it "destroys the requested product", authorized: false do
         product = Product.create! valid_attributes
-        expect {
-          delete :destroy, params: {id: product.to_param}, session: valid_session
-        }.to change(Product, :count).by(0)
+        expect do
+          delete :destroy, params: { id: product.to_param }, session: valid_session
+        end.to change(Product, :count).by(0)
       end
 
       it "returns an unauthorized response", authorized: false do
         product = Product.create! valid_attributes
 
-        delete :destroy, params: {id: product.to_param}, session: valid_session
+        delete :destroy, params: { id: product.to_param }, session: valid_session
         expect_unauthorized
       end
     end
