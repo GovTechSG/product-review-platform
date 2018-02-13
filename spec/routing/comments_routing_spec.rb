113327a67e2905ebbc7b57298decc453c6325887
Wynn Chen <wynn987@gmail.com>
2018-02-13 16:19:40 +0800

    prefix api/v1 to api routes

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe CommentsController, type: :routing do
   describe "routing" do
     it "routes to #show" do
-      expect(get: "/comments/1").to route_to("comments#show", id: "1")
+      expect(get: "/api/v1/comments/1").to route_to("comments#show", id: "1")
     end
   end
 end
--- spec/routing/likes_routing_spec.rb
+++ spec/routing/likes_routing_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe LikesController, type: :routing do
   describe "routing" do
     it "routes to #show" do
-      expect(get: "/likes/1").to route_to("likes#show", id: "1")
+      expect(get: "/api/v1/likes/1").to route_to("likes#show", id: "1")
     end
   end
 end
