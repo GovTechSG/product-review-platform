faef5ac87464a4d93c56c4dca1d6df5766ffa534
Wynn Chen <wynn987@gmail.com>
2018-02-13 16:19:40 +0800

    prefix api/v1 to api routes

--- spec/routing/reviews_routing_spec.rb
+++ spec/routing/reviews_routing_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe ReviewsController, type: :routing do
   describe "routing" do
     it "routes to #show" do
-      expect(get: "/reviews/1").to route_to("reviews#show", id: "1")
+      expect(get: "/api/v1/reviews/1").to route_to("reviews#show", id: "1")
     end
   end
 end
--- spec/routing/services_routing_spec.rb
+++ spec/routing/services_routing_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe ServicesController, type: :routing do
   describe "routing" do
     it "routes to #show" do
-      expect(get: "/services/1").to route_to("services#show", id: "1")
+      expect(get: "/api/v1/services/1").to route_to("services#show", id: "1")
     end
   end
 end
