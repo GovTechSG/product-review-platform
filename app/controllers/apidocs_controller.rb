aeb76d80c268d350187d3327b4449f83249894e5
gohkokboon <guowen4567@gmail.com>
2018-02-13 16:21:45 +0800

    Updated host path

--- app/controllers/apidocs_controller.rb
+++ app/controllers/apidocs_controller.rb
@@ -15,8 +15,8 @@ class ApidocsController < ApplicationController
       end
     end
     # Need to enter the host path of the playground, for now its on localhost:3001
-    key :host, "localhost:3000"
-    # key :host, "#{ENV['SWAGGER_API_BASE_PATH']}"
+    # key :host, "localhost:3000"
+    key :host, "#{ENV['SWAGGER_API_BASE_PATH']}"
     key :consumes, ['application/json']
     key :produces, ['application/json']
 
