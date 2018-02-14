84287dda47eb2ce87dd9110a8e1c6d93b54c193e
gohkokboon <guowen4567@gmail.com>
2018-02-14 18:18:53 +0800

    Moved SwaggerUI to Views folder to enable erb file format

---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Rails.application.config.assets.precompile += %w( swagger-ui.css )
+Rails.application.config.assets.precompile += %w( swagger-ui-bundle.js )
+Rails.application.config.assets.precompile += %w( swagger-ui-standalone-preset.js )
