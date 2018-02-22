7a638ee6b0349b45ac612335444c79b99821acc9
Wynn Chen <wynn987@gmail.com>
2018-02-22 18:56:45 +0800

    Fix admin console (#62)
    Fixed admin console bugs:
    Name should validate for presence
    Name should validate for uniqueness, should not show "An unhandled lowlevel error occurred. The application logs may have details."
    After discarding app, apps still able to use old access tokens to interact with api

--- app/models/app.rb
+++ app/models/app.rb
@@ -7,6 +7,7 @@ class App < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          authentication_keys: [:name]
 
+  validates :name, presence: true, uniqueness: true
   def email_required?
     false
   end
@@ -19,10 +20,23 @@ class App < ApplicationRecord
     false
   end
 
+  def discard
+    super
+    @existing_token = Doorkeeper::AccessToken.find_by(resource_owner_id: id, revoked_at: nil)
+    if @existing_token
+      @existing_token.revoked_at = Time.current
+      @existing_token.save
+    end
+  end
+
   class << self
     def authenticate(name, password)
       app = App.find_for_authentication(name: name)
-      app.try(:valid_password?, password) ? app : nil
+      if app.nil? || app.discarded?
+        nil
+      else
+        app.try(:valid_password?, password) ? app : nil
+      end
     end
   end
 end
