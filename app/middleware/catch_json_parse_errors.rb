c14a3486573c66c8fba6371d35b5b59aa4bb9353
Wynn Chen <wynn987@gmail.com>
2018-02-26 14:08:58 +0800

    added more generic error handling

---
+++ app/middleware/catch_json_parse_errors.rb
@@ -0,0 +1,17 @@
+class CatchJsonParseErrors
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    begin
+      @app.call(env)
+    rescue ActionDispatch::ParamsParser::ParseError => error
+      error_output = "There was a problem in the JSON you submitted: #{error}"
+      return [
+        400, { "Content-Type" => "application/json" },
+        [ { status: 400, error: error_output }.to_json ]
+      ]
+    end
+  end
+end
