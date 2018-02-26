7cd2e29b90eaed3fe668c0dcd8bb187e6a311cc2
gohkokboon <guowen4567@gmail.com>
2018-02-26 15:52:25 +0800

    Updated routing spec file

--- spec/routing/services_routing_spec.rb
+++ spec/routing/services_routing_spec.rb
@@ -2,10 +2,6 @@ require "rails_helper"
 
 RSpec.describe ServicesController, type: :routing do
   describe "routing" do
-    # it "routes to #show" do
-    #   expect(get: "/api/v1/services/1").to route_to("services#show", id: "1")
-    # end
-
     it "routes to #index" do
       expect(get: "api/v1/companies/1/services").to route_to("services#index", company_id: "1")
     end
