fb97fb27178c2a5ef1809b22f8ef2c4f0fd27748
gohkokboon <guowen4567@gmail.com>
2018-03-06 14:52:10 +0800

    Added landing page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,8 @@
 Rails.application.config.assets.precompile += %w(swagger-ui.css)
+Rails.application.config.assets.precompile += %w(bootstrap.min.css)
+Rails.application.config.assets.precompile += %w(one-page-wonder.min.css)
 Rails.application.config.assets.precompile += %w(swagger-ui-bundle.js)
 Rails.application.config.assets.precompile += %w(swagger-ui-standalone-preset.js)
+Rails.application.config.assets.precompile += %w(bootstrap.bundle.min.js)
+Rails.application.config.assets.precompile += %w(jquery.min.js)
+
