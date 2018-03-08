a695481b53b11be020143f0632f11974db1287b6
gohkokboon <guowen4567@gmail.com>
2018-03-08 10:14:57 +0800

    Comments endpoints (#76)
    * completed Comment endpoint
    
    * Fix rubocop
    
    * to merge with latest development branch
    
    * Completed spec for comment endpoint
    
    * changed from find to find_by

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,15 @@ class CommentsController < ApplicationController
   include SwaggerDocs::Comments
   before_action :doorkeeper_authorize!
   before_action :set_comment, only: [:show, :update, :destroy]
+  before_action :validate_comment_pressence, only: [:show, :update, :destroy]
+  before_action :set_review, only: [:index, :create]
+  before_action :validate_review_presence, only: [:index, :create]
+  before_action :set_new_comment, only: [:create]
+  before_action :validate_user_presence, only: [:create]
+
   # GET /reviews/:review_id/comments
   def index
-    @comments = Comment.where(review_id: params[:review_id])
-
+    @comments = Comment.kept.where(review_id: params[:review_id])
     render json: @comments, methods: [:user]
   end
 
@@ -16,12 +21,10 @@ class CommentsController < ApplicationController
 
   # POST /reviews/:review_id/comments
   def create
-    @comment = Comment.new(create_params.merge(review_id: params[:review_id]))
-
-    if @comment.save
-      render json: @comment, status: :created, location: @comment
+    if @comments.save
+      render json: @comments, status: :created, location: @comments
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: @comments.errors, status: :unprocessable_entity
     end
   end
 
@@ -36,13 +39,33 @@ class CommentsController < ApplicationController
 
   # DELETE /comments/1
   def destroy
-    @comment.destroy
+    @comment.discard
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
-      @comment = Comment.find(params[:id])
+      @comment = Comment.find_by(id: params[:id])
+    end
+
+    def set_review
+      @review = Review.find_by(id: params[:review_id])
+    end
+
+    def set_new_comment
+      @comments = Comment.new(create_params.merge(review_id: params[:review_id]))
+    end
+
+    def validate_comment_pressence
+      render_error(404, "Comment id not found") if @comment.nil? || !@comment.presence?
+    end
+
+    def validate_user_presence
+      render_error(404, "User id entered does not exist") if User.find_by(id: @comments.user_id).nil?
+    end
+
+    def validate_review_presence
+      render_error(404, "Review id entered does not exist") if @review.nil? || !@review.presence?
     end
 
     # Only allow a trusted parameter "white list" through.
---
+++ spec/serializers/comment_serializer_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe CommentSerializer, type: :serializer do
+  context 'Individual Resource Representation' do
+    before(:all) do
+      @comment = create(:product_review_comment)
+    end
+
+    subject { CommentSerializer.new(@comment, root: false).as_json["object"] }
+
+    it 'has a content' do
+      expect(subject['content']).to eql(@comment.content)
+    end
+
+    it 'has a user' do
+      expect(subject['user_id']).to eql(@comment.user_id)
+    end
+
+    it 'has a review' do
+      expect(subject['review_id']).to eql(@comment.review_id)
+    end
+  end
+end
