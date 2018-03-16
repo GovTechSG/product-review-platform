68cefcb5eca1e6af66e0499b7f828b3028bebabc
Wynn Chen <wynn987@gmail.com>
2018-03-16 10:28:52 +0800

    fixed rubocop

--- app/models/concerns/swagger_docs/agency.rb
+++ app/models/concerns/swagger_docs/agency.rb
@@ -1,49 +1,49 @@
 module SwaggerDocs::Agency
   extend ActiveSupport::Concern
- 
+
   included do
     include Swagger::Blocks
- 
+
     swagger_schema :Agency do
       key :type, :object
       key :required, [:id, :name, :email, :number]
- 
+
       property :id do
         key :type, :integer
         key :format, :int64
       end
- 
+
       property :name do
         key :type, :string
         key :example, "Ministry of Communications and Information"
       end
- 
+
       property :email do
         key :type, :string
         key :example, "MCI_Connects@mci.gov.sg"
       end
- 
+
       property :number do
         key :type, :string
         key :example, "1800-8379655"
       end
- 
+
       property :kind do
         key :type, :string
         key :example, "Ministry"
       end
- 
+
       property :acronym do
         key :type, :string
         key :example, "MCI"
       end
- 
+
       property :description do
         key :type, :string
         key :example, "The Ministry of Communications and Information (MCI) oversees the development of the infocomm technology, cyber security, media and design sectors; the national library, national archives and public libraries; as well as Government’s information and public communication policies. MCI’s mission is to connect our people to community, government and opportunity, enabled by trustworthy infrastructure and technology."
       end
     end
- 
+
     swagger_schema :AgencyInput do
       allOf do
         schema do
@@ -52,23 +52,23 @@ module SwaggerDocs::Agency
             property :name do
               key :type, :string
             end
- 
+
             property :email do
               key :type, :string
             end
- 
+
             property :number do
               key :type, :string
             end
- 
+
             property :acronym do
               key :type, :string
             end
- 
+
             property :kind do
               key :type, :string
             end
- 
+
             property :description do
               key :type, :string
             end
@@ -77,4 +77,4 @@ module SwaggerDocs::Agency
       end
     end
   end
- end
+end
