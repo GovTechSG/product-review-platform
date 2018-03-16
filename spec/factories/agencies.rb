afaf6ab59b317c608af536ceea45c2bb248b8fd1
Wynn Chen <wynn987@gmail.com>
2018-03-16 10:26:13 +0800

    added missing files from merge

--- spec/factories/agencies.rb
+++ spec/factories/agencies.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :agency do
     name { FFaker::Name.name }
+    acronym { FFaker::NatoAlphabet.code }
+    kind { ["Ministry", "Statutory Board", "Agency"].sample }
+    description { FFaker::Lorem.paragraph }
     email { FFaker::Internet.email }
     number { FFaker::PhoneNumberSG.fixed_line_number }
   end
