59b2387581b857d7527c5f79f1d5105d4e70e144
gohkokboon <guowen4567@gmail.com>
2018-03-27 15:17:42 +0800

    Add commentor commentable (#129)
    * Added commentable polymorphic
    
    * updated comment db schema and updated code to comment index
    
    * Added commenter and commentable
    
    * updated comment endpoint swagger
    
    * resovled index serializer
    
    * Updated and added new spec for comment endpoint
    
    * fix rubocop
    
    * udpate seed file
    
    * Fix minor code issue
    
    * Added type to from

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,7 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :content
-  # belongs_to :commentable, key: "object", serializer: ReviewSerializer, if: :review?
-  # belongs_to :commenter, key: "from", serializer: AgencySerializer, if: :agency?
 
   belongs_to :commentable, key: "object", polymorphic: true
   belongs_to :commenter, key: "from", polymorphic: true
