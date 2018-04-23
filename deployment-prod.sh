1e71e6c007ae487e7d23661ecf664ba0dc60fb8c
Wynn Chen <wynn987@gmail.com>
2018-04-23 12:54:50 +0800

    added missing parameter

--- deployment-prod.sh
+++ deployment-prod.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose-prod.yml pull backend
-docker-compose up -d --build backend
+docker-compose -f docker-compose-prod.yml up -d --build backend
 docker-compose -f docker-compose-prod.yml run backend rake db:migrate
 docker system prune -a -f
