eedd31bf36138c231a78832d40c78b8836bc0b16
Wynn Chen <wynn987@gmail.com>
2018-04-23 10:45:54 +0800

    modified prod scripts

--- deployment-prod.sh
+++ deployment-prod.sh
@@ -1,6 +1,5 @@
 docker-compose -f docker-compose-prod.yml stop backend
 docker-compose -f docker-compose-prod.yml rm backend
 docker-compose -f docker-compose-prod.yml pull backend
-docker-compose -f docker-compose-prod.yml run backend
 docker-compose -f docker-compose-prod.yml run backend rake db:migrate
 docker-compose -f docker-compose-prod.yml run -d --publish 3000:3000 --entrypoint "sh /app/docker-onrun.sh" backend
