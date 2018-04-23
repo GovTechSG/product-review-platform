81d6048daf52acac73b533f993993cadbf275712
Wynn Chen <wynn987@gmail.com>
2018-04-23 13:04:45 +0800

    use down and up instead

--- deployment-prod.sh
+++ deployment-prod.sh
@@ -1,4 +1,6 @@
+docker-compose -f docker-compose-prod.yml down
 docker-compose -f docker-compose-prod.yml pull backend
-docker-compose -f docker-compose-prod.yml up -d --build backend
+docker-compose -f docker-compose-prod.yml up -d --build
 docker-compose -f docker-compose-prod.yml run backend rake db:migrate
+docker-compose -f docker-compose-prod.yml run -d --publish 3000:3000 --entrypoint "sh /app/docker-onrun.sh" backend
 docker system prune -a -f
