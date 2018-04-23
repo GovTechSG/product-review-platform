edc6d438046459ac76ab66d071e3be294e367c90
Wynn Chen <wynn987@gmail.com>
2018-04-23 11:37:09 +0800

    force stop instead of graceful stop

--- deployment-prod.sh
+++ deployment-prod.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose-prod.yml stop backend
+docker-compose -f docker-compose-prod.yml kill backend
 docker-compose -f docker-compose-prod.yml rm backend
 docker-compose -f docker-compose-prod.yml pull backend
 docker-compose -f docker-compose-prod.yml run backend rake db:migrate
