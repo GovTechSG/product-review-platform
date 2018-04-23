c64fb30799dbdf39fffc206aac37b9f4b36c7460
Wynn Chen <wynn987@gmail.com>
2018-04-23 12:52:56 +0800

    modify deployment scripts

--- deployment-prod.sh
+++ deployment-prod.sh
@@ -1,6 +1,4 @@
-docker-compose -f docker-compose-prod.yml kill backend
-docker-compose -f docker-compose-prod.yml rm backend
 docker-compose -f docker-compose-prod.yml pull backend
+docker-compose up -d --build backend
 docker-compose -f docker-compose-prod.yml run backend rake db:migrate
-docker-compose -f docker-compose-prod.yml run -d --publish 3000:3000 --entrypoint "sh /app/docker-onrun.sh" backend
 docker system prune -a -f
