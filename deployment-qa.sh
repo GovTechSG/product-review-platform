9d3bf70acb58b636cc1e10b306c9e7c3089250e3
Ubuntu <ubuntu@ip-10-7-4-250.ap-southeast-1.compute.internal>
2018-05-08 03:08:30 +0000

    fix deployment scripts

--- deployment-qa.sh
+++ deployment-qa.sh
@@ -1,9 +1,9 @@
-docker-compose -f docker-compose-prod.yml down
-docker-compose -f docker-compose-prod.yml pull qa
-docker-compose -f docker-compose-prod.yml pull sandbox
-docker-compose -f docker-compose-prod.yml up -d --build
-docker-compose -f docker-compose-prod.yml run qa rake db:migrate
-docker-compose -f docker-compose-prod.yml run sandbox rake db:migrate
-docker-compose -f docker-compose-prod.yml run -d --publish 3000:3000 --entrypoint "sh /app/docker-onrun.sh" sandbox
-docker-compose -f docker-compose-prod.yml run -d --publish 3000:3002 --entrypoint "sh /app/docker-onrun.sh" qa
+docker-compose -f docker-compose-qa.yml down
+docker-compose -f docker-compose-qa.yml pull qa
+docker-compose -f docker-compose-qa.yml pull sandbox
+docker-compose -f docker-compose-qa.yml up -d --build
+docker-compose -f docker-compose-qa.yml run qa rake db:schema:load db:seed RAILS_ENV=qa DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+docker-compose -f docker-compose-qa.yml run sandbox rake db:migrate RAILS_ENV=production
+docker-compose -f docker-compose-qa.yml run -d --publish 3000:3000 --entrypoint "sh /app/docker-onrun.sh" sandbox
+docker-compose -f docker-compose-qa.yml run -d --publish 3002:3000 --entrypoint "sh /app/docker-onrun.sh" qa
 docker system prune -a -f
