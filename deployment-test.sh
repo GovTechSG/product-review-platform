953b2ba5805a97a384a41c1829e440cf93dfbf95
Wynn Chen <wynn987@gmail.com>
2018-05-23 10:23:28 +0800

    added fix to scripts

--- deployment-test.sh
+++ deployment-test.sh
@@ -2,4 +2,4 @@ docker-compose -f docker-compose-test.yml down
 docker-compose -f docker-compose-test.yml up --build -d
 docker-compose -f docker-compose-test.yml run backend rake db:create
 docker-compose -f docker-compose-test.yml run backend rake db:schema:load
-docker-compose -f docker-compose-test.yml run -d --publish 3000:3000 --entrypoint "sh /app/docker-ontest.sh" backend
+docker-compose -f docker-compose-test.yml run --publish 3000:3000 --entrypoint "/app/docker/docker-entrypoint-test.sh" backend
