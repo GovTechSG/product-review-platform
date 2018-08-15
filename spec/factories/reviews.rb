5f29ab4e9ba475f46b8d10f699c381759c0c2277
Wynn Chen <wynn987@gmail.com>
2018-08-15 16:03:02 +0800

    fixed review factory

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :review do
-    score { rand(1..10) }
+    score { rand(1..3) }
     content { FFaker::Lorem.paragraph }
     association :reviewer, factory: :company
     grant
