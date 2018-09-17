4297138f52874d2d32cee7678454970285480ec2
Wynn Chen <wynnchen@Wynns-MacBook-Pro.local>
2018-09-17 16:11:33 +0800

    can projects.companies.kept

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < Reviewable
   include SwaggerDocs::Project
   has_many :company_reviewable, as: :reviewable, dependent: :destroy
-  has_many :companies, through: :company_reviewables
+  has_many :companies, through: :company_reviewable, as: :reviewable
 
-  validates :name, :reviews_count, presence: true
+  validates_presence_of :name, :description, :reviews_count
   validates :description, presence: true, allow_blank: true
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,7 @@
 class Service < Reviewable
   include SwaggerDocs::Service
   has_many :company_reviewable, as: :reviewable, dependent: :destroy
-  has_many :companies, through: :company_reviewables, as: :reviewable
+  has_many :companies, through: :company_reviewable, as: :reviewable
 
   validates_presence_of :name, :description, :reviews_count
   validates :description, presence: true, allow_blank: true
