d36683537de41ddb2ccd2185511bcea02a35adfe
Wynn Chen <wynnchen@ip-192-168-109-250.ap-southeast-1.compute.internal>
2018-09-18 17:09:57 +0800

    fix grant specs

--- spec/controllers/grants_controller_spec.rb
+++ spec/controllers/grants_controller_spec.rb
@@ -50,15 +50,15 @@ RSpec.describe GrantsController, type: :controller do
     describe "GET #index company" do
       it "returns a success response", authorized: true do
         review = create(:product_review)
-        get :index, params: { company_id: review.reviewable.company.hashid }
+        get :index, params: { company_id: review.reviewable.companies.first.hashid }
 
         expect(response).to be_success
       end
 
       it "does not return grants from deleted company", authorized: true do
         review = create(:product_review)
-        review.reviewable.company.discard
-        get :index, params: { company_id: review.reviewable.company.hashid }
+        review.reviewable.companies.first.discard
+        get :index, params: { company_id: review.reviewable.companies.first.hashid }
         expect(response.status).to eq(404)
       end
 
@@ -361,7 +361,7 @@ RSpec.describe GrantsController, type: :controller do
     describe "GET #index company" do
       it "returns a nunauthorized response", authorized: false do
         review = create(:product_review)
-        get :index, params: { company_id: review.reviewable.company_id }
+        get :index, params: { company_id: review.vendor_id }
 
         expect_unauthorized
       end
