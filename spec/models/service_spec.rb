87152d52d827a1c949f5ea39616a00ea3559648d
Wynn Chen <wynnchen@ip-192-168-109-75.ap-southeast-1.compute.internal>
2018-09-20 17:46:46 +0800

    fix reviewable spec

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'concerns/statistics/score_aggregator_spec.rb'
 
 RSpec.describe Service, type: :model do
 
@@ -11,18 +12,55 @@ RSpec.describe Service, type: :model do
       expect(build(:service, name: nil)).not_to be_valid
     end
 
-    it 'is invalid without a description' do
-      expect(build(:service, description: nil)).not_to be_valid
-    end
-
     it "is not valid without a reviews_count" do
-      service = build(:company)
+      service = build(:service)
       service.reviews_count = nil
       expect(service).to_not be_valid
     end
+  end
 
-    it 'is invalid without a company' do
-      expect(build(:service, company: nil)).not_to be_valid
+  describe 'aggregations' do
+    it 'updates review count of the company on create' do
+      service = create(:service)
+      company = service.companies.create! build(:company_as_params)
+      expect do
+        service.reviews.create!(build(:service_review, vendor_id: company.id).attributes)
+      end.to change { service.companies.first.reviews_count }.by(1)
+    end
+
+    it 'updates review count of the service on review discard' do
+      service = create(:service)
+      company = service.companies.create! build(:company_as_params)
+      service.reviews.create!(build(:service_review, vendor_id: company.id).attributes)
+      service.reviews.create!(build(:service_review, vendor_id: company.id).attributes)
+      expect do
+        service.reviews.first.discard
+      end.to change { service.companies.first.reviews_count }.by(-1)
+    end
+    it 'returns 0 when there are no reviews' do
+      service = create(:service)
+      company = service.companies.create! build(:company_as_params)
+      service.reviews.create!(build(:service_review, vendor_id: company.id).attributes)
+      service.reviews.first.discard
+      expect(service.companies.first.reviews_count).to eq(0)
+    end
+    it 'updates score on service discard' do
+      service = create(:service)
+      company = service.companies.create! build(:company_as_params)
+      service.reviews.create!(build(:service_review, vendor_id: company.id).attributes)
+      undiscarded_service = company.services.create!(build(:service).attributes)
+      undiscarded_service.reviews.create!(build(:service_review, vendor_id: company.id).attributes)
+      service.discard
+
+      expect(company.aggregate_score).to eq(undiscarded_service.aggregate_score)
+    end
+    it 'returns 0 when there are no score' do
+      service = create(:service)
+      service.companies.create! build(:company_as_params)
+      service.reviews.create!(build(:service_review).attributes)
+      service.reviews.first.discard
+
+      expect(service.companies.first.aggregate_score).to eq(0)
     end
   end
 end
